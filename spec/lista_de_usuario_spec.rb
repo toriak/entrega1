70ae66bd427e1799dc2dcf16bef3667ff8f30547
toriak <raff.94.sma@gmail.com>
2015-04-12 20:08:49 -0300

    modifico lista_de_usuario_spec.rb se agrego los test de agregar_usuario

--- spec/lista_de_usuario_spec.rb
+++ spec/lista_de_usuario_spec.rb
@@ -11,4 +11,30 @@ describe ListaDeUsuario do
 			expect(lista.lista_de_usuarios).to eq([])
 		end
 	end
+
+	describe "agregar_usuario" do
+
+		lista = ListaDeUsuario.new
+
+		it "agrega un Usuario nuevo" do
+
+			lista.agregar_usuario (Usuario.new "raul", 22)
+
+			expect(lista.buscar "raul",22).to be(true)
+		end
+
+		it "no agrega otro objeto a la lista" do
+
+=begin	
+			cuando se le envia el mensaje a la lista de agregar esta al terminar de ejecutarce devuelve
+				-true si es que se a agregado correctamente
+				-false si no se a agregado
+=end 
+			resultado = lista.agregar_usuario (ListaDeUsuario.new)
+			expect(resultado).to be(false)
+			
+		end
+	end
+
+
 end
