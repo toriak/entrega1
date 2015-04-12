dd7ff6b67212b456f3c9eb04f4f90846eae4029b
toriak <raff.94.sma@gmail.com>
2015-04-12 17:24:21 -0300

    modificado lista_de_usuario

--- Clases/lista_de_usuarios.rb
+++ Clases/lista_de_usuarios.rb
@@ -5,18 +5,27 @@ class ListaDeUsuario
 	def initialize
 		@lista_de_usuarios = []
 
-		@lista_de_usuarios << usuario.new("andres",1234)
 	end
 
-	def buscar(nombre_de_usuario, contraseña)
+	def agregar_usuario (unUsuario)
+		
+		if unUsuario.class == Usuario
+			@lista_de_usuarios << unUsuario
+			return true
+		else
+			return false
+		end
+	end
+
+	def buscar(nombre_de_usuario, password)
 
 		@lista_de_usuarios.each do |usuario_de_lista|
 			
-			if usuario_de_lista.nombre_de_usuario = nombre_de_usuario
+			if usuario_de_lista.verificacion_nombre nombre_de_usuario
 			
 				usuario = usuario_de_lista	
 
-				return usuario.contraseña = contraseña
+				return usuario.verificacion_password password
 			end	
 
 		end
