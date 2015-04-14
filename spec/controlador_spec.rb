7da0c28828b58062c01d101fa2cd8464aad9f58d
toriak <raff.94.sma@gmail.com>
2015-04-14 11:03:33 -0300

    creacion controlador_spec.rb

---
+++ spec/controlador_spec.rb
@@ -0,0 +1,16 @@
+requiere_relative '../Clases/controlador.rb'
+
+describe Controlador do
+
+	describe "initialize" do
+		
+	end
+
+	describe "buscar" do
+	
+	end
+
+	describe "agregar" do
+
+	end	
+end
