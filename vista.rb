18b48b38055afc47efe3c61a98c30d0575c0b727
toriak <raff.94.sma@gmail.com>
2015-04-17 16:21:29 -0300

    modifico *vista.rb __ se agrego ña opcion registras usuario al menu de los usuarios no logueados y se corrigio la opcion del menu salir

--- vista.rb
+++ vista.rb
@@ -5,38 +5,29 @@ class Vista
 	def initialize(unControlador)
 		@controlador = unControlador
 		self.vista_de_usuario_deslogueado
+    @salir = false;
 	end
-	
+
 	def vista_de_usuario_deslogueado
 
-		salir = false;
-			while !salir do
+			while !@salir do
 				say "Seleccione una accion:"
   				choose do |menu|
 
     			menu.choice(:Login){
+    				@salir = self.accion_menu_login
+   				}
 
-    				nombre_usuario = ask("nombre de usuario: ",String)
-
-    				password = ask("Password: ",String){ 
-    					|password| password.echo = "·"
-    				}
-
-    				if @controlador.existe_usuario(nombre_usuario,password)
-
-    					say "\t Usted se ha logueado exitosamente!" 
-    					salir = self.vista_de_usuario_logueado
-    				else
-    					say "\tNombre de usuario o contraseña incorrecta \n"
-    				end
-   				}				
+          menu.choice(:registrar){
+            self.accion_menu_registrar
+          }
 
     			menu.choice(:Estado) do
 	    			say("\t Tu estado es deslogueado\n")
     			end
 
     			menu.choice(:Salir) {
-      				salir = true
+      				@salir = true
       				say("\t Adios, vuelva pronto \n")
     			}
     		end
@@ -45,15 +36,14 @@ class Vista
 
 	def vista_de_usuario_logueado
 
-		salir = false;
-			while !salir do
+			while !@salir do
 				say "Seleccione una accion:"
   				choose do |menu|
 
     			menu.choice(:Logout){
     				say("\t Usted se ha deslogueado en forma exitosa\n")
     				return false
-    			}				
+    			}
 
     			menu.choice(:Estado) do
 	    			say("\t Tu estado es logueado\n")
@@ -66,4 +56,38 @@ class Vista
     		end
   		end
 	end
+
+  def accion_menu_login
+
+    nombre_usuario = ask("nombre de usuario: ",String)
+
+      password = ask("Password: ",String){
+        |password| password.echo = "·"
+      }
+
+      if @controlador.existe_usuario?(nombre_usuario,password)
+
+        say "\t Usted se ha logueado exitosamente!"
+        @salir = self.vista_de_usuario_logueado
+      else
+        say "\tNombre de usuario o contraseña incorrecta \n"
+      end
+  end
+
+  def accion_menu_registrar
+
+    nombre_usuario = ask("nombre de usuario: ",String)
+
+      password = ask("Password: ",String){
+        |password| password.echo = "·"
+      }
+
+      if @controlador.agregar_usuario(nombre_usuario,password)
+
+        say "\t Usted se ha registrado exitosamente!"
+      else
+        say "\tNombre de usuario ocupado \n"
+      end
+
+  end
 end
