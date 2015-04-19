9064b981fd028e7f08c5b1e7fdf0422c8d965a0e
toriak <raff.94.sma@gmail.com>
2015-04-19 14:57:45 -0300

    modifico *lista_de_usuario __ se realizo test de los mensajes de cifrado_texto_plano cifrado_caesar_cipher cifrado_bcrypt

--- spec/lista_de_usuario_spec.rb
+++ spec/lista_de_usuario_spec.rb
@@ -1,4 +1,7 @@
 require_relative '../Clases/lista_de_usuarios.rb'
+require_relative '../Clases/texto_plano.rb'
+require_relative '../Clases/caesar_cipher.rb'
+require_relative '../Clases/bcrypt.rb'
 
 describe ListaDeUsuario do
 
@@ -43,6 +46,33 @@ describe ListaDeUsuario do
 		it "encuentra a un usuario que no esta en la lista" do
 			resultado = lista.existe_nombre_usuario"Jose"
 			expect(resultado).to be(false)
+		end
+	end
+
+	describe "cifrado_texto_plano" do
+
+		it "que cambie el tipo de codificador a texto plano desde texto plano" do
+			lista.cifrado_texto_plano
+			codificador_de_lista = lista.codificador
+			expect(codificador_de_lista.class).to be(TextoPlano)
+		end
+	end
+
+	describe "cifrado_caesar_cipher" do
+
+		it "que cambie el tipo de codificador a texto plano desde texto plano" do
+			lista.cifrado_caesar_cipher
+			codificador_de_lista = lista.codificador
+			expect(codificador_de_lista.class).to be(CifradoCesar)
+		end
+	end
+
+	describe "cifrado_bcrypt" do
+
+		it "que cambie el tipo de codificador a texto plano desde texto plano" do
+			lista.cifrado_bcrypt
+			codificador_de_lista = lista.codificador
+			expect(codificador_de_lista.class).to be(BcrypCifrado)
 		end
 	end
 
